5ff3a906bdc7eee04a04873b4042ed46209f07b7
Ignacio <ignacio.becher@gmail.com>
2022-08-23 00:18:01 -0300

    first commit

---
+++ cursoJava.java
@@ -0,0 +1,30 @@
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!?INTRODUCCION?!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+//Hello world
+class cursoJava {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!?TIPOS DE
+// DATOS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+// ----------------DECLARACION DE VARIABLES---------------------//
+// Int
+int version; // int se usa para enteros
+version=9;
+
+// ----------------TIPOS DE DATOS---------------------//
+
+// Int: sirve para enteros
+int posicion = 10;
+
+// Float: numeros decimales
+float decimal = 5.9f;// siempre lleva una f al final
+
+// Char: letras o caracteres unicos
+char letra="a"
+char letra="97"// codigo para "a"
+
+// Boolean:
+boolean valor=true
